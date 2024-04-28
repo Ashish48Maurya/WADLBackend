5195def8743af5db1e6f370c6d3c46da6c1d497e
Ashish  Maurya <118504361+Ashish48Maurya@users.noreply.github.com>
2024-04-28 23:29:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 app.use(cors({
     // origin: 'http://localhost:3000',   //for localMachine
-    origin: 'https://eventease18.netlify.app',  //for Production
+    origin: '*',  //for Production
     credentials: true,
 }));
 
